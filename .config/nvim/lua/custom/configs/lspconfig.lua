f9b77b4c765bf1a85d50a4f943aab3390772aaa5
pooyan <thepooyan@gmail.com>
2024-04-05 20:39:18 +0330

    neovim config into loop

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 --
 -- -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd" }
+local servers = { "html", "cssls", "tsserver", "clangd", "tailwindcss", "tsserver" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -30,14 +30,4 @@ lspconfig.emmet_ls.setup({
         },
       },
     }
-})
-
-lspconfig.tsserver.setup({
-  on_attach = on_attach,
-  capabilities = capabilities
-});
-
-lspconfig.tailwindcss.setup({
-  on_attach = on_attach,
-  capabilities = capabilities
 })
