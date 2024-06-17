3592e4ff092e91e83fecafae8983e65ecdbbf370
pooyan <thepooyan@gmail.com>
2024-06-17 17:17:37 +0330

    rebuild neovim

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,10 +1,9 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
---
-local lspconfig = require "lspconfig"
---
--- -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "tailwindcss", "tsserver" }
+
+local lspconfig = require("lspconfig")
+
+local servers = {"tsserver", "tailwindcss", "eslint", "cssls"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -12,22 +11,3 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
-
--- 
--- lspconfig.pyright.setup { blabla}
-
--- -- emmet
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-lspconfig.emmet_ls.setup({
-    -- on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
-    init_options = {
-      html = {
-        options = {
-          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-          ["bem.enabled"] = true,
-        },
-      },
-    }
-})
