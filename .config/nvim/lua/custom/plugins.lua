45ceb39a34c86337ec69cd7d93e398bcc5d75e25
pooyan <thepooyan@gmail.com>
2024-07-30 15:00:32 +0330

    obsidian-nvim: formatter adds name, date and time

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -175,6 +175,30 @@ local plugins = {
       note_id_func = function(title)
         return title or ""
       end,
+      -- Optional, alternatively you can customize the frontmatter data.
+      ---@return table
+      note_frontmatter_func = function(note)
+        -- Add the title of the note as an alias.
+        if note.title then
+          note:add_alias(note.title)
+        end
+
+        local currentDate = os.date("%Y-%m-%d")
+        local currentTime = os.date("%H:%M")
+
+        local out = { name = note.id, date = currentDate, time = currentTime }
+
+
+        -- `note.metadata` contains any manually added fields in the frontmatter.
+        -- So here we just make sure those fields are kept in the frontmatter.
+        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+          for k, v in pairs(note.metadata) do
+            out[k] = v
+          end
+        end
+
+        return out
+      end,
     },
   }
 }
