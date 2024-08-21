97eeb742edacb5f29e99c27bd17d16315211d6be
pooyan <thepooyan@gmail.com>
2024-08-21 17:17:16 +0330

    added nvim chatgpt

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -5,6 +5,20 @@ local plugins = {
 
   -- Override plugin definition options
 
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  },
+
   -- vim tumux navigator
   {
     "christoomey/vim-tmux-navigator",
