630e268603135d779380fe633d820d85a92c3797
pooyan <thepooyan@gmail.com>
2024-08-22 12:35:11 +0330

    nvim: chatGPT basic setup (that dosen't work with ollama)

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -9,7 +9,27 @@ local plugins = {
     "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
     config = function()
-      require("chatgpt").setup()
+      require("chatgpt").setup({
+        api_host_cmd = "echo http://127.0.0.1:11434",
+        api_key_cmd = 'echo -n "a"',
+        openai_params = {
+          model = "codellama:7b",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          max_tokens = 300,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        },
+        openai_edit_params = {
+          model = "codellama:7b",
+          frequency_penalty = 0,
+          presence_penalty = 0,
+          temperature = 0,
+          top_p = 1,
+          n = 1,
+        }
+      })
     end,
     dependencies = {
       "MunifTanjim/nui.nvim",
