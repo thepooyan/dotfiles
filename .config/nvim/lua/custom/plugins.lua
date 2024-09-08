bd20e6ecda7a52665641fc547fa2cb26fb33b8dd
pooyan <thepooyan@gmail.com>
2024-09-09 01:20:22 +0330

    added nice nvim

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -2,12 +2,28 @@ local overrides = require("custom.configs.overrides")
 
 ---@type NvPluginSpec[]
 local plugins = {
+	-- lazy.nvim
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
 	{
 		"echasnovski/mini.nvim",
 		version = false,
 		event = "VeryLazy",
 		config = function()
-      local spec_treesitter = require('mini.ai').gen_spec.treesitter
+			local spec_treesitter = require("mini.ai").gen_spec.treesitter
 			require("mini.ai").setup({
 				custom_textobjects = {
 					F = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
