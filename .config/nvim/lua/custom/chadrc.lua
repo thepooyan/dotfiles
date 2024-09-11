50a464b532f38ac48c1f06b4e5074b57126a6574
pooyan <thepooyan@gmail.com>
2024-09-12 00:44:49 +0330

    beatiful tap dance for open and close symbols

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -4,8 +4,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "radium",
-  theme_toggle = { "radium", "one_light" },
+  theme = "catppuccin",
+  theme_toggle = { "catppuccin", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
