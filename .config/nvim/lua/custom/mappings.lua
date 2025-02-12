db9c66d35bae76ffc0348ca4c7f62c209f23c93a
pooyan <thepooyan@gmail.com>
2025-02-12 23:50:46 +0330

    close all tabs in nvim

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -11,6 +11,7 @@ M.general = {
     ["<S-h>"] = {"F h", "jump backwards by spaces"},
     ["<leader>]"] = { ":Gen<CR>", "" },
     ["<leader>df"] = { "<cmd>NoiceDismiss<CR>", ""},
+    ["<leader>X"] = { ":tabdo %bd<CR>", ""},
     -- ["<leader>q"] = { "yi\"di\"hdlcl{`<Esc>\"0pi`}", ""},
   },
   v = {
