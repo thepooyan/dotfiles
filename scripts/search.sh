0883ef120fd2f7c4bef1d377775131f6b0edcab4
pooyan <thepooyan@gmail.com>
2025-03-02 16:10:16 +0330

    search with bangs

--- scripts/search.sh
+++ scripts/search.sh
@@ -3,5 +3,5 @@ query=$(rofi -dmenu -p "Search Google:")
 if [ -n "$query" ]; then
   folan=$(echo "$query" | sed 's/ /+/g')
   # xdg-open --new-window "https://www.google.com/search?q=$folan"
-  vivaldi --new-window "https://www.google.com/search?q=$folan"
+  vivaldi --new-window "https://unduck.link?q=$folan"
 fi
